e8d9ac8ffb16af3b8081594e028c30f195209ec3
shubs98 <shubprasain@hotmail.com>
2023-01-03 15:24:50 +0100

    Finished React in Gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,45 +7,38 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql,Link } from "gatsby"
+import { container,nav,navLinks,navLinkItem,navLinkText } from "./layout.module.css"
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({pageTitle,children }) => {
+  
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div className={container}>
+     <title>{pageTitle}</title>
+     <nav className={nav}>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+              </Link>
+              </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/about">
+              About
+              </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/maserati">
+              Maserati
+              </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
   )
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Definieer je component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+    <p>Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry. </p>
+    </Layout>
+  )
+}
+
+// Stap 3: Exporteer je component
+export default AboutPage
