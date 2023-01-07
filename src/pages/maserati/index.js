1d4a71f887da7d40d5ac389f9162e45eab01d8aa
shubs98 <shubprasain@hotmail.com>
2023-01-07 23:26:11 +0100

    finished

--- src/pages/maserati/index.js
+++ src/pages/maserati/index.js
@@ -1,37 +1,78 @@
 import * as React from 'react'
 import {graphql, Link} from 'gatsby'
 import Layout from '../../components/layout'
+import { getImage ,GatsbyImage} from 'gatsby-plugin-image'
+import Maserati from '../../components/maserati'
+import {description,hero,maseratis,section,subtitle} from '../page.module.css'
 
 
 // Step 2: Definieer je component
-const MaseratiPage = ({data: {allWpCar: { edges }}}) => {
+const MaseratiPage = ({
+  data: {
+    allWpCar: { edges },
+    wpPage: {maseratiField},
+  },
+}) => {
+  const image = getImage(maseratiField.picture.localFile)
   return (
+    
     <Layout>
-    {edges.map((item) => {
+      <GatsbyImage
+      className={hero}
+      image={image}
+      alt={maseratiField.picture.altText}
+      />
+      <section className={section}>
+        <h2 className={subtitle}>{maseratiField.title}</h2>
+        <div className={description} dangerouslySetInnerHTML={{__html: maseratiField.description,}}
+        />
+       <div className={maseratis}>
+       {edges.map((item) => {
       const maserati = item.node.maserati;
       const slug = item.node.slug;
       return <Link to={`/maserati/${slug}`}>
         <p key={item.node.id}>{maserati.title}</p>
       </Link>
     })}
-
-    
-    
-    </Layout>
+        </div> 
+        
+      </section>
+     </Layout>
   )
 }
 
 export const query = graphql`
-query {
+query  {
+  wpPage(slug: {eq: "maserati"}) {
+    maseratiField {
+      description
+      title
+      picture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+          }
+        }
+        altText
+      }
+    }
+  }
   allWpCar {
     edges {
       node {
         maserati {
           title
-          description
+          image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+              }
+            }
+            altText
+          }
         }
-        id
         slug
+        id
       }
     }
   }
